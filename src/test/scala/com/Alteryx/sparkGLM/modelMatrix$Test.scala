1e97f03ac924ad3c17a66c1ba44eaf1b2af70b88
cafreeman <cfreeman@alteryx.com>
2015-06-14 12:35:07 -0500

    Fix `testData` and update `modelMatrix` test
    Changed the `testData` object to hold values instead of functions that return `DataFrame`s.
    
    Also dropped `SparkContext` and `SQLContext` imports in favor of the built-in `TestSQLContext`.
    
    Now multiple test suites can run without having akka errors for using non-unique pointers.

--- src/test/scala/com/Alteryx/sparkGLM/modelMatrix$Test.scala
+++ src/test/scala/com/Alteryx/sparkGLM/modelMatrix$Test.scala
@@ -1,17 +1,14 @@
 package com.Alteryx.sparkGLM
 
-import org.apache.spark.{SparkConf, SparkContext}
-import org.apache.spark.sql.SQLContext
+import org.apache.spark.sql.test.TestSQLContext
 import org.scalatest.FunSuite
 import com.Alteryx.testUtils.data.testData._
 
 class modelMatrix$Test extends FunSuite {
-  val conf = new SparkConf().setMaster("local").setAppName("modelMatrixTest")
-  val sc = new SparkContext(conf)
-  val sqlCtx = new SQLContext(sc)
+  val sqlCtx = TestSQLContext
 
   test("modelMatrixWithMixedTypes") {
-    val testDF = modelMatrix(dummyDF(sqlCtx))
+    val testDF = modelMatrix(dummyDF)
     assert(testDF.columns.length == 4)
     val expectedCols = Array("intField", "strField_b", "strField_c", "numField")
     assert(expectedCols.forall { elem =>
@@ -21,7 +18,7 @@ class modelMatrix$Test extends FunSuite {
   }
 
   test("modelMatrixWithNumOnly") {
-    val testDF = modelMatrix(dummyDF(sqlCtx).select("numField", "intField"))
+    val testDF = modelMatrix(dummyDF.select("numField", "intField"))
     assert(testDF.columns.length == 2)
     val expectedCols = Array("numField", "intField")
     assert(expectedCols.forall { elem =>
@@ -31,7 +28,7 @@ class modelMatrix$Test extends FunSuite {
   }
 
   test("modelMatrixWithStrOnly") {
-    val testDF = modelMatrix(dummyDF(sqlCtx).select("strField"))
+    val testDF = modelMatrix(dummyDF.select("strField"))
     assert(testDF.columns.length == 2)
     val expectedCols = Array("strField_b", "strField_c")
     assert(expectedCols.forall { elem =>
@@ -41,7 +38,7 @@ class modelMatrix$Test extends FunSuite {
   }
 
   test("modelMatrixLinearRegData") {
-    val rawDF = mixedDF(sqlCtx).select("intercept", "x1", "x2", "x3", "x4", "x5", "x6", "x7", "y")
+    val rawDF = mixedDF.select("intercept", "x1", "x2", "x3", "x4", "x5", "x6", "x7", "y")
     val testDF = modelMatrix(rawDF)
     assert(testDF.columns.length == 10)
     val expectedCols = Array("intercept", "x1", "x2", "x3", "x4", "x5", "x6", "x7_b", "x7_c", "y")
