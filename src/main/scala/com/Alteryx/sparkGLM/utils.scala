e5a1ed6998625cf7ea3d45b71f48bf9a1ac5d534
dputler <dan.putler@gmail.com>
2015-06-26 14:59:24 -0700

    Adding utility methods for obtaining weighted least squares fits and cleaned up some variable names in the LM method.

--- src/main/scala/com/Alteryx/sparkGLM/utils.scala
+++ src/main/scala/com/Alteryx/sparkGLM/utils.scala
@@ -3,7 +3,9 @@ package com.Alteryx.sparkGLM
 import breeze.linalg._
 import breeze.numerics._
 
-import edu.berkeley.cs.amplab.mlmatrix.RowPartitionedMatrix
+import edu.berkeley.cs.amplab.mlmatrix.{NormalEquations, RowPartitionedMatrix}
+import edu.berkeley.cs.amplab.mlmatrix.util.Utils
+import edu.berkeley.cs.amplab.mlmatrix.NormalEquations._
 
 import org.apache.spark.sql.{DataFrame, Column}
 import org.apache.spark.sql.functions.{col, lit}
@@ -11,6 +13,10 @@ import org.apache.spark.rdd.RDD
 import org.apache.spark.sql.types.DoubleType
 
 object utils {
+
+  //
+  // DataFrame utility functions
+  //
 
   def matchCols (estDF: DataFrame, scoreDF: DataFrame): DataFrame = {
     val missingCols: Array[Column] = estDF.columns.diff(scoreDF.columns).map { field =>
@@ -35,6 +41,11 @@ object utils {
     dataArray(0)
   }
 
+
+  //
+  // Modelling support methods
+  //
+
   // From ml-matrix, the reducer function to sum together the partitons of X'X
   // and X'y
   def reduceNormal(
@@ -44,6 +55,59 @@ object utils {
     a._2 :+= b._2
     a
   }
+
+  // A case class for a minimal weighted least squares fit object
+  case class WLSObj(coefs: DenseMatrix[Double], diagDesign: DenseVector[Double])
+
+  // A method to do a weighted least squares fit to data in a single partition
+  def wlsSingle (
+      X: DenseMatrix[Double],
+      y: DenseMatrix[Double],
+      w: DenseMatrix[Double]): WLSObj = {
+    val W = diag(w.toDenseVector)
+    val XtWXi = inv(X.t * W * X)
+    val XtWy = X.t * W * y
+    val coefs = XtWXi * XtWy
+    val diagDesign = sqrt(diag(XtWXi))
+    new WLSObj(coefs = coefs, diagDesign = diagDesign)
+  }
+
+  // A method to get the coponents needed to do WLS on RowPartionedMatrix objects
+  def wlsComponents (
+      X: RowPartitionedMatrix,
+      y: RowPartitionedMatrix,
+      w: RowPartitionedMatrix): (DenseMatrix[Double], DenseMatrix[Double]) = {
+    val Xyw = X.rdd.zip(y.rdd).zip(w.rdd).map {
+      case((a, b), c) => (a.mat, b.mat, c.mat)
+    }
+    val XtWX_XtWy = Xyw.map { part =>
+      (part._1.t * diag(part._3.toDenseVector) * part._1,
+        part._1.t * diag(part._3.toDenseVector) * part._2)
+    }
+
+    val treeBranchingFactor = X.rdd.context.getConf.getInt("spark.mlmatrix.treeBranchingFactor", 2).toInt
+    val depth = math.ceil(math.log(XtWX_XtWy.partitions.size)/math.log(treeBranchingFactor)).toInt
+    val reduced = edu.berkeley.cs.amplab.mlmatrix.util.Utils.treeReduce(XtWX_XtWy, utils.reduceNormal, depth=depth)
+
+    reduced
+  }
+
+  // A method to do a weighted least squares fit to data in multiple partitions
+  def wlsMultiple (
+      X: RowPartitionedMatrix,
+      y: RowPartitionedMatrix,
+      w: RowPartitionedMatrix): WLSObj = {
+    val components = wlsComponents(X, y, w)
+    val XtWXi = inv(components._1)
+    val coefs = XtWXi * components._2
+    val diagDesign = sqrt(diag(XtWXi))
+    new WLSObj(coefs = coefs, diagDesign = diagDesign)
+  }
+
+
+  //
+  // Printing support methods
+  //
 
   // A method to round a number to a desired number of decimal places
   def roundDigits(num: Double, digits: Int): Double = {
